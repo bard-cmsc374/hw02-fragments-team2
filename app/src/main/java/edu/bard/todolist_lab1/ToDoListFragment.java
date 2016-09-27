bd1b34105a82d57a539d3006e09ddb01a28fa88d
Jason Zisheng Chang <j.zisheng@gmail.com>
2016-09-26 20:44:55 -0400

    1.1
    Added EditText Field

---
+++ app/src/main/java/edu/bard/todolist_lab1/ToDoListFragment.java
@@ -0,0 +1,49 @@
+package edu.bard.todolist_lab1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+/**
+ * Created by Jason on 9/26/2016.
+ */
+
+public class ToDoListFragment extends Fragment {
+    private EditText mTitleField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_todolist, container, false);
+
+        mTitleField = (EditText)v.findViewById(R.id.add_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        return v;
+    }
+
+}
